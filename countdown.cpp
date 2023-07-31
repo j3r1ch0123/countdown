0e67789f038a2ed0f421db0919c15e9318849fcb
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2023-07-31 19:03:39 -0400

    Add files via upload

--- countdown.cpp
+++ countdown.cpp
@@ -21,30 +21,30 @@ void countdown() {
         cout << "How many seconds? ";
         cin >> seconds; // Input seconds
 
-        while (true) {
+        while (seconds >= 0) {
             cout << "Seconds: " << seconds << endl; // Display time remaining
             seconds--; // Reduce seconds
             usleep(second); // Wait a second
-            system("clear"); // Clear the screen and repeat, change to "cls" if using windows
+            system("clear"); // Clear the screen and repeat
             
             if (seconds == 0) { // When the timer's done
                 break;
             }
         }
     }
-    if (choice == "minutes" || choice == "Minutes") {
+    else if (choice == "minutes" || choice == "Minutes") {
         cout << "How many minutes? " << endl; // Input minutes
         cin >> minutes;
         cout << "How many seconds? " << endl; // Input seconds
         cin >> seconds;
 
-        while (true) {
+        while (minutes >= 0 && seconds >= 0) {
             // Display time remaining
             cout << "Minutes: " << minutes << endl;
             cout << "Seconds: " << seconds << endl;
             seconds--; // Reduce seconds
             usleep(second); // Wait a second
-            system("clear");
+            system("clear"); // Change to cls if on Windows
 
             // Reduce the number of minutes after 60 seconds
             if (seconds < 0) {
@@ -58,36 +58,17 @@ void countdown() {
         }
         }
     }
-
-    if (choice == "hours" || choice == "Hours") {
+    
+    else if (choice == "hours" || choice == "Hours") {
         cout << "How many hours? " << endl; // Input hours
         cin >> hours;
         cout << "How many minutes? " << endl; // Input minutes
         cin >> minutes;
         cout << "How many seconds? " << endl; // Input seconds
         cin >> seconds;
-
-        while(true) {
-            cout << "Hours: " << hours << endl;
-            cout << "Minutes: " << minutes << endl;
-            cout << "Seconds: " << seconds << endl;
-            seconds--; // Reduce seconds
-            usleep(second); // Wait a second
-            system("clear"); // Clear the screen
-
-            if (seconds < 0) {
-                minutes--; // Reduce minutes
-                seconds = 59; // Reset seconds
-            }
-
-            if (minutes < 0) {
-                hours--; // Reduce hours
-                minutes = 59; // Reset minutes
-        }
-    }
     }
     
-    if (choice == "days" || choice == "Days") {
+    else if (choice == "days" || choice == "Days") {
         cout << "How many days? " << endl; //Input days
         cin >> days;
         cout << "How many hours? " << endl; //Input hours
@@ -96,8 +77,9 @@ void countdown() {
         cin >> minutes;
         cout << "How many seconds? " << endl; //Input seconds
         cin >> seconds;
+    }
 
-        while (true) {
+        while (days >= 0 && hours >= 0 && minutes >= 0 && seconds >= 0) {
             // Display time remaining
             cout << "Days: " << days << endl;
             cout << "Hours: " << hours << endl;
@@ -117,19 +99,20 @@ void countdown() {
                 minutes = 59; // Reset minutes
             
             if (hours < 0) {
-                days--; // Reduce days
-                hours = 23; // Reset hours
+                days--; //Reduce days
+                hours = 23; //Reset hours
+                minutes = 59; //Reset minutes
+                seconds = 59; //Reset seconds
             }
 
             }
             // When the timer is done
-            if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+            if (hours == 0 && minutes == 0 && seconds == 0) {
                 break;
             }
         }
     }
-}
-    
+
 int main() {
     
     countdown(); // Call the function
